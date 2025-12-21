e371220ddf471baf4b96d9f7873268b2668d68d2
CJ Duan <Data.Scientist@dulun.com>
2025-12-21 16:09:17 -0500

    feat: Introduce new notebooks and update `utils.py` to include Anscombe's Quartet and interactive datasaurus plots, replacing previous birthday paradox utilities.

--- content/ipynb/utils.py
+++ content/ipynb/utils.py
@@ -1,216 +1,137 @@
 import numpy as np
-from datetime import timedelta, date
+import pandas as pd
+import os
 import matplotlib.pyplot as plt
-import seaborn as sns
+from IPython.display import display
 import ipywidgets as widgets
-from ipywidgets import interact_manual
+from ipywidgets import interact,HBox, VBox
+import matplotlib.gridspec as gridspec
 
+df_anscombe = pd.read_csv('df_anscombe.csv')
+df_datasaurus = pd.read_csv("datasaurus.csv")
 
-
-class your_bday:
-    def __init__(self) -> None:
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+def plot_anscombes_quartet():
+    fig, axs = plt.subplots(2,2, figsize = (8,5), tight_layout = True)
+    i = 1
+    fig.suptitle("Anscombe's quartet", fontsize = 16)
+    for line in axs:
+        for ax in line:
+            ax.scatter(df_anscombe[df_anscombe.group == i]['x'],df_anscombe[df_anscombe.group == i]['y'])
+            ax.set_title(f'Group {i}')
+            ax.set_ylim(2,15)
+            ax.set_xlim(0,21)
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            i+=1
         
-        self.fig = fig
-        self.ax = ax1
-        self.ax_hist = ax2
-        self.dates = [
-            (date(2015, 1, 1) + timedelta(days=n)).strftime("%m-%d") for n in range(365)
-        ]
-        self.match = False
-        self.bday_str = None
-        self.bday_index = None
-        self.n_students = 0
-        self.history = []
-        self.bday_picker = widgets.DatePicker(description="Pick your bday", disabled=False, style={'description_width': 'initial'})
-        self.start_button = widgets.Button(description="Simulate!")
+def display_widget():
 
-        display(self.bday_picker)
-        display(self.start_button)
+    dropdown_graph_1 = widgets.Dropdown(
+    options=df_datasaurus.group.unique(),
+    value='dino',
+    description='Data set 1: ',
+    disabled=False,
+)
+    
+    statistics_graph_1 = widgets.Button(
+    value=False,
+    description='Compute stats',
+    disabled=False,
+    button_style='',
+    tooltip='Description',
+    icon='' 
+)
 
-        self.start_button.on_click(self.on_button_clicked)
+    dropdown_graph_2 = widgets.Dropdown(
+    options=df_datasaurus.group.unique(),
+    value='h_lines',
+    description='Data set 2: ',
+    disabled=False,
+)
+    
+    statistics_graph_2 = widgets.Button(
+    value=False,
+    description='Compute stats',
+    disabled=False,
+    button_style='',
+    tooltip='Description',
+    icon='' 
+)
+    plotted_stats_graph_1 = None
+    plotted_stats_graph_2 = None
 
-    def on_button_clicked(self, b):
-        self.match = False
-        self.n_students = 0
-
-        self.get_bday()
-        self.add_students()
-
-    def get_bday(self):
-        try:
-            self.bday_str = self.bday_picker.value.strftime("%m-%d")
-        except AttributeError:
-            self.ax.set_title(f"Input a valid date and try again!")
+    fig = plt.figure(figsize = (8,4), tight_layout = True)
+    gs = gridspec.GridSpec(2,2)
+    ax_1 = fig.add_subplot(gs[0,0])
+    ax_2 = fig.add_subplot(gs[1,0])
+    ax_text_1 = fig.add_subplot(gs[0,1])
+    ax_text_2 = fig.add_subplot(gs[1,1])
+    df_group_1 = df_datasaurus.groupby('group').get_group('dino')
+    df_group_2 = df_datasaurus.groupby('group').get_group('h_lines')
+    sc_1 = ax_1.scatter(df_group_1['x'],df_group_1['y'], s = 4)
+    sc_2 = ax_2.scatter(df_group_2['x'],df_group_2['y'], s = 4)
+    ax_1.set_xlabel('x')
+    ax_1.set_ylabel('y')
+    ax_2.set_xlabel('x')
+    ax_2.set_ylabel('y')
+    ax_text_1.axis('off')
+    ax_text_2.axis('off')
+    
+    def dropdown_choice(value, plotted_stats, ax_text, sc):
+        if value.new != plotted_stats:
+            ax_text.clear()
+            ax_text.axis('off')
+        sc.set_offsets(df_datasaurus.groupby('group').get_group(value.new)[['x', 'y']])
+        fig.canvas.draw_idle()
+    
+        
+    def get_stats(value, plotted_stats, ax_text, dropdown, val):
+        value = dropdown.value
+        if value == plotted_stats:
             return
-        self.bday_index = self.dates.index(self.bday_str)
+        ax_text.clear()
+        ax_text.axis('off')
+        df_group = df_datasaurus.groupby('group').get_group(value)
+        means = df_group.mean()
+        var = df_group.var()
+        corr = df_group.corr()
+        ax_text.text(0,
+                    0,
+                    f"Statistics:\n      Mean x:      {means['x']:.2f}\n      Variance x: {var['x']:.2f}\n\n      Mean y:      {means['y']:.2f}\n      Variance y: {var['y']:.2f}\n\n      Correlation:  {corr['x']['y']:.2f}"
+                    )
+        if val == 1:
+            plotted_stats_graph_1 = value
+        if val == 2:
+            plotted_stats_graph_2 = value
+        
+        
+        
 
-
-    def generate_bday(self):
-        # gen_bdays = np.random.randint(0, 365, (n_people))
-        gen_bday = np.random.randint(0, 365)
-        # if not np.isnan(self.y[gen_bday]):
-        if gen_bday == self.bday_index:
-            self.match = True
+    dropdown_graph_1.observe(lambda value: dropdown_choice(value,plotted_stats_graph_1, ax_text_1, sc_1), names = 'value')
+    statistics_graph_1.on_click(lambda value: get_stats(value, plotted_stats_graph_1, ax_text_1, dropdown_graph_1,1))
+    dropdown_graph_2.observe(lambda value: dropdown_choice(value,plotted_stats_graph_2, ax_text_2, sc_2), names = 'value')
+    statistics_graph_2.on_click(lambda value: get_stats(value, plotted_stats_graph_2, ax_text_2, dropdown_graph_2,2))    
+    graph_1_box = HBox([dropdown_graph_1, statistics_graph_1])
+    graph_2_box = HBox([dropdown_graph_2, statistics_graph_2])
+    display(VBox([graph_1_box,graph_2_box]))
     
-    def add_students(self):
 
-        if not self.bday_str:
-            return
+def plot_datasaurus():
 
-        while True:
-            if self.match:
-                self.history.append(self.n_students)
-#                 print(f"Match found. It took {self.n_students} students to get a match")
-                n_runs = [i for i in range(len(self.history))]
-                self.ax.scatter(n_runs, self.history)
-                # counts, bins = np.histogram(self.history)
-                # plt.stairs(counts, bins)
-                # self.ax_hist.hist(bins[:-1], bins, weights=counts)
-                self.ax_hist.clear()
-                sns.histplot(data=self.history, ax=self.ax_hist, bins=16)
-                # plt.show()
-                break
+    fig, axs = plt.subplots(6,2, figsize = (7,9), tight_layout = True)
+    i = 0
+    fig.suptitle("Datasaurus", fontsize = 16)
+    for line in axs:
+        for ax in line:
+            if i > 12:
+                ax.axis('off')
+            else:
+                group = df_datasaurus.group.unique()[i]
+                ax.scatter(df_datasaurus[df_datasaurus.group == group]['x'],df_datasaurus[df_datasaurus.group == group]['y'], s = 4)
+                ax.set_title(f'Group {group}')
+                ax.set_ylim(-5,110)
+                ax.set_xlim(10,110)
+                ax.set_xlabel('x')
+                ax.set_ylabel('y')
+                i+=1
 
-            self.generate_bday()
-            self.n_students += 1
-            self.ax.set_title(f"Match found. It took {self.n_students} students.\nNumber of runs: {len(self.history)+1}")
-            # self.fig.canvas.draw()
-            # self.fig.canvas.flush_events()
-
-
-big_classroom_sizes = [*range(1,1000, 5)]
-small_classroom_sizes = [*range(1, 80)]
-
-def plot_simulated_probs(sim_probs, class_size):
-    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
-#     ax.scatter(class_size, sim_probs)
-    sns.scatterplot(x=class_size, y=sim_probs, ax=ax, label="simulated probabilities")
-    ax.set_ylabel("Simulated Probability")
-    ax.set_xlabel("Classroom Size")
-    ax.set_title("Probability vs Number of Students")
-    ax.plot([0, max(class_size)], [0.5, 0.5], color='red', label="p = 0.5")
-    ax.grid(which = 'minor', color = '#EEEEEE', linewidth = 0.8)
-    ax.minorticks_on()
-    ax.legend()
-    plt.show()
-
-    
-    
-class third_bday_problem:
-    def __init__(self) -> None:
-        fig, axes = plt.subplot_mosaic(
-            [["top row", "top row"], ["bottom left", "bottom right"]], figsize=(10, 8)
-        )
-        self.fig = fig
-        self.ax = axes["top row"]
-        self.count_ax = axes["bottom left"]
-        self.ax_hist = axes["bottom right"]
-        self.ax.spines["top"].set_color("none")
-        self.ax.spines["right"].set_color("none")
-        self.ax.spines["left"].set_color("none")
-        self.ax.get_yaxis().set_visible(False)
-        x = np.arange(365)
-        y = np.zeros((365,))
-        y[y == 0] = np.nan
-
-        y_match = np.zeros((365,))
-        y_match[y_match == 0] = np.nan
-
-        self.x = x
-        self.y = y
-        self.y_match = y_match
-        self.match = False
-        self.n_students = 0
-
-        self.dates = [
-            (date(2015, 1, 1) + timedelta(days=n)).strftime("%m-%d") for n in range(365)
-        ]
-        self.month_names = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ]
-
-        self.history = []
-        self.match_index = None
-        self.match_str = None
-
-        self.cpoint = self.fig.canvas.mpl_connect("button_press_event", self.on_button_clicked)
-
-        # self.start_button = widgets.Button(description="Simulate!")
-
-        # display(self.start_button)
-
-        # self.start_button.on_click(self.on_button_clicked)
-
-    def generate_bday(self):
-        gen_bday = np.random.randint(0, 365)
-
-        if not np.isnan(self.y[gen_bday]):
-            self.match_index = gen_bday
-            self.match_str = self.dates[gen_bday]
-            self.y_match[gen_bday] = 1
-            self.match = True
-
-        self.y[gen_bday] = 0.5
-
-    def on_button_clicked(self, event):
-        if event.inaxes in [self.ax]:
-            self.new_run()
-            self.add_students()
-
-    def add_students(self):
-
-        while True:
-            if self.match:
-                self.history.append(self.n_students)
-                n_runs = [i for i in range(len(self.history))]
-                self.count_ax.scatter(n_runs, self.history)
-                self.count_ax.set_ylabel("# of students")
-                self.count_ax.set_xlabel("# of simulations")
-
-                month_str = self.month_names[int(self.match_str.split("-")[0]) - 1]
-                day_value = self.match_str.split("-")[1]
-                self.ax.set_title(
-                    f"Match found for {month_str} {day_value}\nIt took {self.n_students} students to get a match"
-                )
-                self.ax_hist.clear()
-                sns.histplot(data=self.history, ax=self.ax_hist, bins="auto")
-                break
-
-            self.generate_bday()
-            self.n_students += 1
-            self.ax.set_title(f"Number of students: {self.n_students}")
-
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
-
-            if not np.isnan(self.y_match).all():
-                markerline, stemlines, baseline = self.ax.stem(
-                    self.x, self.y_match, markerfmt="*"
-                )
-                plt.setp(markerline, color="green")
-                plt.setp(stemlines, "color", plt.getp(markerline, "color"))
-                plt.setp(stemlines, "linestyle", "dotted")
-            self.ax.stem(self.x, self.y, markerfmt="o")
-
-    def new_run(self):
-        y = np.zeros((365,))
-        y[y == 0] = np.nan
-        y_match = np.zeros((365,))
-        y_match[y_match == 0] = np.nan
-        self.y_match = y_match
-        self.y = y
-        self.n_students = 0
-        self.match = False
-        self.ax.clear()
